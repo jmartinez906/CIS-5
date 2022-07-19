ae33eb38a0f997f78cddd9f1c6d4527fca4c85b7
jmartinez906 <109406245+jmartinez906@users.noreply.github.com>
2022-07-18 23:11:37 -0700

    Add files via upload

---
+++ Class/Hello World/main.cpp
@@ -0,0 +1,45 @@
+/* 
+ * File:   main.cpp
+ * Author: Jonathan
+ * Created on June 23, 2022, 7:26 PM
+ * Purpose: Our First Program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Mathematical/Physics/Conversions, Higher dimensioned arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map inputs to outputs -> The Process
+    
+    //Display Results
+    cout<<"Hello World"<<endl;
+    //Another way to do the same thing;
+    cout<<"Hello "<<"World"<<endl;
+    //Another way to do the same thing;
+    cout
+            <<"Hello "
+            <<"World"
+            <<endl;
+    //Another way to do the same thing;
+    cout<<"Hello ";
+    cout<<"World";
+    cout<<endl;
+    //Exit stage right
+    return 0;
+}
+
---
+++ Class/Math/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Jonathan
+ * Created on June 23, 2022, 7:26 PM
+ * Purpose: 
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Mathematical/Physics/Conversions, Higher dimensioned arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    short op1,op2,result;
+    
+    //Initialize Variables
+    op1=20000;
+    op2=30000;
+    
+    //Map inputs to outputs -> The Process
+    result=op1+op2;
+    
+    //Display Results
+    cout<<result<<" = "<<op1<<" + "<<op2<<endl;
+    
+    //Exit stage right
+    return 0;
+}
+
